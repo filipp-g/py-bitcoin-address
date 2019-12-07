e2137ec4f50b1951287c0e5ac643ab8319f96db9
Filipp G <8738425+filipp-g@users.noreply.github.com>
2019-12-06 19:57:54 -0500

    add bitcoin scripts

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # py-crypto-address
 Python scripts to simply and easily generate cryptocurrency addresses
+
+```
+secretkey = SigningKey.generate(curve=SECP256k1)
+publickey = secretkey.get_verifying_key()
+
+wif = make_wif(secretkey.to_string())
+address = make_address(serialize_pk(publickey))
+
+print("WIF: " + wif.decode("utf-8"))
+print("Address: " + address.decode("utf-8"))
+```
+
+Tested on Python 3.8.0
+
+## Disclaimer
+USE AT OWN RISK
---
+++ bitcoin.py
@@ -0,0 +1,42 @@
+from hashlib import sha256, new
+from ecdsa import SigningKey, SECP256k1
+from ecdsa.util import number_to_string
+from base58 import b58encode
+
+
+def serialize_pk(pubkey, compress=False):
+    if compress:
+        p = pubkey.pubkey
+        pfx = b'\x03' if p.point.y() & 1 else b'\x02'
+        str = pfx + number_to_string(p.point.x(), p.order)
+    else:
+        str = b'\x04' + pubkey.to_string()
+    return str
+
+
+def get_checksum(addr):
+    return sha256(sha256(addr).digest()).digest()[:4]
+
+
+def make_address(pubkey):
+    hash160 = new("ripemd160")
+    hash160.update(sha256(pubkey).digest())
+    addr = b"\x00" + hash160.digest()
+    addr += get_checksum(addr)
+    return b58encode(addr)
+
+
+def make_wif(privkey):
+    wif = b"\x80" + privkey
+    wif += get_checksum(wif)
+    return b58encode(wif)
+
+
+secretkey = SigningKey.generate(curve=SECP256k1)
+publickey = secretkey.get_verifying_key()
+
+wif = make_wif(secretkey.to_string())
+address = make_address(serialize_pk(publickey))
+
+print("WIF: " + wif.decode("utf-8"))
+print("Address: " + address.decode("utf-8"))
